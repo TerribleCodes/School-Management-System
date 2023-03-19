bba6b3f16f82565bac2b973e95659d74422822ea
TerribleCodes <madawadesilva9998@gmail.com>
2023-03-19 13:09:53 +0530

    Added Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CounterClass from "./components/counter.class";
+import CounterFunction from "./components/counter.function";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterClass />
+      <CounterFunction />
+    </div>
+  );
+}
+
+export default App;
